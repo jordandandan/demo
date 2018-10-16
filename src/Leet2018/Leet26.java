c646001ae09bbebc15caff015f9c5ed6a3b5bfa1
bjxujiayi <bjxujiayi@corp.netease.com>
2018-10-16 19:49:41 +0800

    Leet

---
+++ src/Leet2018/Leet26.java
@@ -0,0 +1,24 @@
+package Leet2018;
+
+/**
+ * @author bjxujiayi
+ * @create 2018-10-16 19:48
+ **/
+public class Leet26 {
+    public int removeDuplicates(int[] nums) {
+        int prev = 0, after = 1;
+        if(nums.length < 2){
+            return nums.length;
+        }
+        while(after<nums.length){
+            if(nums[after] == nums[prev]){
+                after++;
+                continue;
+            }else{
+                nums[++prev] = nums[after];
+                after++;
+            }
+        }
+        return prev+1;
+    }
+}
